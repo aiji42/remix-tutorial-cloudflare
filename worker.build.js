aa7a3f95bc832cf71b80b6bc27140768fd1cf082
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-11-26 17:06:38 +0900

    custom worker builder

--- worker.build.js
+++ worker.build.js
@@ -1,19 +1,13 @@
-/* package.json
-"scripts": {
-  "build:worker": "NODE_ENV=production node worker.build.js",
-  "dev:worker": "node worker.build.js",
-  "start": "miniflare --build-command \"npm run dev:worker\" --watch"
-}
-*/
-
-// worker.build.js
 const alias = require('esbuild-plugin-alias');
+
+const isProd = process.env.NODE_ENV === 'production'
 
 require('esbuild')
   .build({
     entryPoints: ['./worker'],
     bundle: true,
     sourcemap: true,
+    minify: isProd,
     outdir: 'dist',
     define: {
       "process.env.NODE_ENV": `"${process.env.NODE_ENV ?? 'development'}"`,

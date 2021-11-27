d9cb598c85d41235cda66b7eaa3ea17f57e6d1e3
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-11-27 13:39:22 +0900

    add prettierrc

--- app/routes/album.$id.tsx
+++ app/routes/album.$id.tsx
@@ -1,17 +1,27 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData } from "remix";
-import {db} from "~/utils/db.server";
+import type { MetaFunction, LoaderFunction } from 'remix'
+import { useLoaderData } from 'remix'
+import { db } from '~/utils/db.server'
 
 type Data = {
-  data: {name: string, cover: string, artists: {id: string, name: string}[], songs: {id: string, name: string, length: number, interactions: {playCount: number}[]}[]}
-};
+  data: {
+    name: string
+    cover: string
+    artists: { id: string; name: string }[]
+    songs: {
+      id: string
+      name: string
+      length: number
+      interactions: { playCount: number }[]
+    }[]
+  }
+}
 
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
 // to the component that renders it.
 // https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = async ({ params: { id} }) => {
-  if (!id) throw new Response("Not Found", { status: 404 });
+export let loader: LoaderFunction = async ({ params: { id } }) => {
+  if (!id) throw new Response('Not Found', { status: 404 })
 
   const data = await db.album.findUnique({
     where: {
@@ -41,18 +51,18 @@ export let loader: LoaderFunction = async ({ params: { id} }) => {
     }
   })
 
-  if (!data) throw new Response("Not Found", { status: 404 });
+  if (!data) throw new Response('Not Found', { status: 404 })
 
   return { data }
-};
+}
 
 // https://remix.run/api/conventions#meta
 export let meta: MetaFunction = () => {
   return {
-    title: "Remix Starter",
-    description: "Welcome to remix!"
-  };
-};
+    title: 'Remix Starter',
+    description: 'Welcome to remix!'
+  }
+}
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
@@ -62,19 +72,39 @@ export default function Index() {
       <div className="flex">
         <img className="mr-6" src={data.cover} />
         <div className="flex flex-col justify-center">
-          <h4 className="mt-0 mb-2 uppercase text-gray-500 tracking-widest text-xs">Albam</h4>
+          <h4 className="mt-0 mb-2 uppercase text-gray-500 tracking-widest text-xs">
+            Albam
+          </h4>
           <h1 className="mt-0 mb-2 text-white text-4xl">{data.name}</h1>
 
-          <p className="text-gray-600 text-sm">Created by {data.artists.map(({ id, name }) => <a key={id}>{name}</a>)} - {data.songs.length} songs, {timeFormattedString(data.songs.reduce((res, { length }) => res + length, 0))}</p>
+          <p className="text-gray-600 text-sm">
+            Created by{' '}
+            {data.artists.map(({ id, name }) => (
+              <a key={id}>{name}</a>
+            ))}{' '}
+            - {data.songs.length} songs,{' '}
+            {timeFormattedString(
+              data.songs.reduce((res, { length }) => res + length, 0)
+            )}
+          </p>
         </div>
       </div>
 
       <div className="mt-6 flex justify-between">
         <div className="flex">
-          <button className="mr-2 bg-green-500 text-green-100 block py-2 px-8 rounded-full">Play</button>
-          <button className="mr-2 border border-white block p-2 rounded-full"><img
-            src="https://image.flaticon.com/icons/svg/2485/2485986.svg" height="25" width="25" /></button>
-          <button className="mr-2 border border-white block p-2 rounded-full">...</button>
+          <button className="mr-2 bg-green-500 text-green-100 block py-2 px-8 rounded-full">
+            Play
+          </button>
+          <button className="mr-2 border border-white block p-2 rounded-full">
+            <img
+              src="https://image.flaticon.com/icons/svg/2485/2485986.svg"
+              height="25"
+              width="25"
+            />
+          </button>
+          <button className="mr-2 border border-white block p-2 rounded-full">
+            ...
+          </button>
         </div>
         <div className="text-gray-600 text-sm tracking-widest text-right">
           <h5 className="mb-1">Followers</h5>
@@ -84,34 +114,43 @@ export default function Index() {
 
       <div className="mt-10">
         <div className="flex text-gray-600">
-          <div className="p-2 w-8 flex-shrink-0"></div>
-          <div className="p-2 w-8 flex-shrink-0"></div>
+          <div className="p-2 w-8 flex-shrink-0" />
+          <div className="p-2 w-8 flex-shrink-0" />
           <div className="p-2 w-full">Title</div>
           <div className="p-2 w-full">Played</div>
           <div className="p-2 w-12 flex-shrink-0 text-right">⏱</div>
         </div>
         {data.songs.map((song) => (
-          <div key={song.id} className="flex border-b border-gray-800 hover:bg-gray-800">
+          <div
+            key={song.id}
+            className="flex border-b border-gray-800 hover:bg-gray-800"
+          >
             <div className="p-3 w-8 flex-shrink-0">▶️</div>
             <div className="p-3 w-8 flex-shrink-0">❤️</div>
             <div className="p-3 w-full">{song.name}</div>
-            <div className="p-3 w-full">{song.interactions.reduce((res, { playCount }) => res + playCount, 0).toLocaleString()}</div>
-            <div className="p-3 w-12 flex-shrink-0 text-right">{timeFormattedStringShort(song.length)}</div>
+            <div className="p-3 w-full">
+              {song.interactions
+                .reduce((res, { playCount }) => res + playCount, 0)
+                .toLocaleString()}
+            </div>
+            <div className="p-3 w-12 flex-shrink-0 text-right">
+              {timeFormattedStringShort(song.length)}
+            </div>
           </div>
         ))}
       </div>
     </div>
-  );
+  )
 }
 
 const timeFormattedString = (inSec: number) => {
-  const sec = ('0' + Math.ceil(inSec % 60)).slice(-2);
-  const min = ('0' + Math.ceil(inSec / 60)).slice(-2);
-  return `${min} min ${sec} sec`;
+  const sec = ('0' + Math.ceil(inSec % 60)).slice(-2)
+  const min = ('0' + Math.ceil(inSec / 60)).slice(-2)
+  return `${min} min ${sec} sec`
 }
 
 const timeFormattedStringShort = (inSec: number) => {
-  const sec = ('0' + Math.ceil(inSec % 60)).slice(-2);
-  const min = ('0' + Math.ceil(inSec / 60)).slice(-2);
-  return `${min}:${sec}`;
+  const sec = ('0' + Math.ceil(inSec % 60)).slice(-2)
+  const min = ('0' + Math.ceil(inSec / 60)).slice(-2)
+  return `${min}:${sec}`
 }
